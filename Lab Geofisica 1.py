13c23af6663a665dc1f09ee7b0099692dbcb2192
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-08-21 09:53:52 -0500

    Update Lab Geofisica 1.py

--- Lab Geofisica 1.py
+++ Lab Geofisica 1.py
@@ -1,16 +1,11 @@
 
 # coding: utf-8
 
-# In[21]:
-
-
 get_ipython().run_line_magic('pylab', 'inline')
 import numpy as np
 import math
 
-
-# In[41]:
-
+####Punto A
 
 G=6.67e-11
 pi=np.pi
@@ -18,24 +13,16 @@ roS=2700
 roE=4000
 
 
-# In[56]:
-
-
-x=linspace(-50,50,1001) #crea un arreglo
-
-
-# In[57]:
+x=linspace(-50,50,501) #crea un arreglo
 
 
 def funcionG(r,z):
     g=(4/3)*pi*G*(r**3)*(roE-roS)*(z/((x**2+z**2)**(3/2)))
     return g
 
+####Punto B
 
-# In[78]:
-
-
-#graf 1
+#graf 2
 z1=20
 r=3
 y1=funcionG(r,z1)
@@ -54,8 +41,7 @@ plt.xlabel("x (m)")
 plt.ylabel("g (m/s^2)")
 
 
-# In[89]:
-
+####Punto C
 
 #Max g1 medio
 mitad1=max(y1)/2
@@ -64,14 +50,5 @@ mitad2=max(y2)/2
 #Max g3 medio
 mitad3=max(y3)/2
 
-y1_restado=abs(y1-mitad1)    
-
-
-# In[92]:
-
-
-for i in range (min(x),max(x)):
-    if y1==mitad1:
-        print (x[i])
-    i+=1
-
+y1_restado=abs(y1-mitad1)
+min(y1_restado)
